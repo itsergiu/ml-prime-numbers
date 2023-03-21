34aa1167dcf5bc41bc338ef32f642251c1f891a6
Sergiu IT <itsergiu@yahoo.com>
2023-03-21 11:49:11 +0200

    Delete prime_lenovo.bat

--- prime_lenovo.bat
+++
@@ -1,6 +0,0 @@
-set venv=prime
-set vdir="G:\My Drive\10 prime"
-call cd %vdir%
-call %USERPROFILE%\Anaconda3.2022.10.64\Scripts\activate %USERPROFILE%\Anaconda3.2022.10.64
-call conda activate %venv%
-call jupyter notebook
